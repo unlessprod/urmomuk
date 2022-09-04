16f50616d4d4b4b27a5e5399e538381442aca94c
unlessprod <103318610+unlessprod@users.noreply.github.com>
2022-09-04 09:49:20 +0300

    Create config.php

---
+++ config/config.php
@@ -0,0 +1,29 @@
+<?php
+//EN - FR
+//CONFIGURATION - CONFIG
+//SITE NAME - Nom du site
+$sitename = 'xd';
+// CAPS ONLY - Majuscule seulement
+
+//Bit or byte - Bit ou octet
+// US=bit and/et EU= octet
+$zone = 'US';
+
+//Data transferred or received  -  Donnée transférée ou recus
+//RX = received/recus TX = transferred/transféerée
+$datatype = 'RX';
+
+//Language - Langue
+//FR = francais/french - EN = english/anglais
+$lang = 'EN';
+
+//Name of interface - Nom de l'interface
+//Usually : 'eth0' - Generalement :'eth0'
+$interface = 'eth0';
+
+// Ip of your server
+$ip = "127.0.0.1";
+$port = "80";
+
+//Graph title
+$Layer7Title = "Layer 7 ==> vxdv.cf";
